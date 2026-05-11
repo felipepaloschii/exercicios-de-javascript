25d461c43744b85c9ce405b5dc689e5a4e6bd8ae
´´felipepaloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-05-11 19:47:18 -0300

    add - ex11

--- ex11.js
+++ ex11.js
@@ -1,1 +1,38 @@
+function ex11() {
 
+   alert("Folha de pagamento");
+   let salario = Number(prompt("Informe seu salário:"));
+
+   if(salario < 1000) {
+   let desconto = salario*0.08
+   let liquido = salario - desconto
+      alert(
+      "Folha de pagamento\n" +
+      "Salário bruto: " + salario + "\n" +
+      "Taxa de desconto: 8% \n" +
+      "Desconto calculado: " + desconto + "\n" +
+      "Salário líquido: " + liquido
+   );
+   } else if(salario > 1000 && salario <= 1500) {
+   let desconto = (salario*8.5)/100
+   let liquido = salario - desconto
+   alert(
+      "Folha de pagamento\n" +
+      "Salário bruto: " + salario + "\n" +
+      "Taxa de desconto: 8,5% \n" +
+      "Desconto calculado: " + desconto + "\n" +
+      "Salário líquido: " + liquido
+   );
+   }else{
+   let desconto = salario*0.09
+   let liquido = salario - desconto
+   alert(
+      "Folha de pagamento\n" +
+      "Salário bruto: " + salario + "\n" +
+      "Taxa de desconto: 9% \n" +
+      "Desconto calculado: " + desconto + "\n" +
+      "Salário líquido: " + liquido
+   );
+   }
+   
+}
