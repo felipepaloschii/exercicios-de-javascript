38e555a1900ad3dc9a74bcc5668391c89183b943
´´felipepaloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-05-11 19:38:22 -0300

    add - ex08 e 09

---
+++ ex08.js
@@ -0,0 +1,13 @@
+function duas_variaveis() {
+
+    let A = 7;
+    let B = 12;
+
+    alert("A: " + A);
+    alert("B: " + B );
+
+    alert("Diferença entre A e B: " + (A-B));
+    alert("O dobro de A somado ao triplo da B: " + (A+A) + (B+B+B));
+    alert("Multiplicação de A e B: " + (A*B));
+    
+}
---
+++ ex09.js
@@ -0,0 +1,10 @@
+function maior_menor() {
+
+    let A = 7;
+    let B = 12;
+
+    alert("A: " + A);
+    alert("B: " + B);
+
+    alert("Do maior pro menor: 12, 11, 10, 9, 8, 7");
+}
