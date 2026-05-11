4a39b607cf8612f9b3bf0de551e5b9d927c1ac41
´´felipepaloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-05-11 19:14:47 -0300

    add - ex5

---
+++ ex05.js
@@ -0,0 +1,22 @@
+function par_impar {
+    let numeros = [];
+
+    numeros = Number(prompt(´´Digite um Número:``))
+
+    if (numeros % 2 == 0) {
+
+        alert(´´Esse número é par``)
+
+        numeros++
+
+        alert (´´Mas agora o número é impar, se tornando: `` + numeros);
+
+    } else {
+
+        alert(´´Esse número é impar``)
+
+        numeros++
+
+        alert(´´Mas agora o número é par, se tornando: `` + numeros);
+    }
+}
