dbe282f88e39b88679d539b0721581d00f2e0910
´´felipepaloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-05-11 19:22:54 -0300

    add - ex06

---
+++ ex06.js
@@ -0,0 +1,18 @@
+function vogal_consoante() {
+
+    let vogal = "A";
+    let vogal2 = "E";
+    let vogal3 = "I";
+    let vogal4 = "O";
+    let vogal5 = "U";
+    let letra; 
+
+    letra = prompt("Digite uma letra:");
+
+    if ((letra == vogal)||(letra == vogal2)||(letra == vogal3)||(letra == vogal4)||(letra == vogal5)) {
+        alert("Essa letra é uma vogal");
+     }else{
+        alert("Essa letra é uma consoante");
+     }
+
+}
