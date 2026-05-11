868426f72db7205564b7586cf3a1248dbf88ae92
´´felipepaloschi <felipe_paloschi@estudante.sesisenai.org.br>
2026-05-11 19:16:31 -0300

    add - arrumei aspas do js

--- ex05.js
+++ ex05.js
@@ -1,22 +1,22 @@
-function par_impar {
+function par_impar() {
     let numeros = [];
 
-    numeros = Number(prompt(´´Digite um Número:``))
+    numeros = Number(prompt("Digite um Número:"))
 
     if (numeros % 2 == 0) {
 
-        alert(´´Esse número é par``)
+        alert("Esse número é par")
 
         numeros++
 
-        alert (´´Mas agora o número é impar, se tornando: `` + numeros);
+        alert ("Mas agora o número é impar, se tornando: " + numeros);
 
     } else {
 
-        alert(´´Esse número é impar``)
+        alert("Esse número é impar")
 
         numeros++
 
-        alert(´´Mas agora o número é par, se tornando: `` + numeros);
+        alert("Mas agora o número é par, se tornando: " + numeros)
     }
 }
